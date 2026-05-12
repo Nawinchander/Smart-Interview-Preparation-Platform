94e2e426a9dfaf0990aaf0b8ed925f5177e81508
Nawin chander <nawinnapzy289@gmail.com>
2026-05-12 10:36:22 +0530

    add

--- interview-platform/server/Src/Services/cacheServices.js
+++ interview-platform/server/Src/Services/cacheServices.js
@@ -7,3 +7,17 @@ redis.createClient();
 client.connect();
 
 module.exports = client;
+
+
+
+/// cache example
+
+// const cachedQuestions =
+// await client.get('questions');
+
+// if (cachedQuestions) {
+
+//   return res.json(
+//     JSON.parse(cachedQuestions)
+//   );
+// }
